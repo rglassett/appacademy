b844a0ad61420967389a0a229a07a101db091aa3
Ryan Glassett <rglassett@gmail.com>
2015-04-20 10:44:07 -0700

    Refactor w1d1/arrays.rb

--- w1/w1d1/arrays.rb
+++ w1/w1d1/arrays.rb
@@ -1,90 +1,132 @@
 class Array
-  def my_uniq
-    solution = []
-    self.each do |x|
-      solution << x unless solution.include?(x)
+  def my_transpose
+    transposed = []
+    each_index do |i|
+      column = []
+      each do |row|
+        column << row[i]
+      end
+      transposed << column
     end
-    solution
+    transposed
   end
-  
+
+  def my_uniq!
+    i, seen = 0, {}
+    until i == length
+      if seen[self[i]]
+        slice!(i)
+      else
+        seen[self[i]] = true
+        i += 1
+      end
+    end
+    self
+  end
+
+  def my_uniq
+    dup.my_uniq!
+  end
+
   def two_sum
     solution = []
-    (0..self.length - 2).each do |i|
-      (i+1..self.length - 1).each do |j|
+    (0...self.length - 1).each do |i|
+      (i + 1...self.length).each do |j|
         if self[i] + self[j] == 0
           solution << [i, j]
-        end    
+        end
       end
     end
     solution
   end
 end
 
-def tower
-  tow1 = [4, 3, 2, 1]
-  tow2 = []
-  tow3 = []
-  gameboard = {'1' => tow1, '2' => tow2, '3' => tow3}
-  
-  while tow2.length < 4 && tow3.length < 4
-    print_board(tow1, tow2, tow3)
-    puts "What tower do you want to move from?"
-    source = gameboard[gets.chomp]
-    puts "What tower do you want to send to?"
-    destination = gameboard[gets.chomp]
-    
-    if is_valid?(source, destination)
-      destination << source.pop
-    else
-      puts "Invalid move."
-      next
-    end     
+class InvalidMoveError < StandardError; end
+
+class TowersOfHanoi
+  attr_reader :towers
+
+  def self.build_towers(size = 3)
+    [(1..size).to_a, [], []]
   end
-end
 
-def is_valid? (source, destination)
-  return false if source.length == 0
-  array = Array.new(destination)
-  array << source[-1]
-  array == array.sort.reverse
-end
+  def initialize(towers = nil)
+    @towers = towers || self.class.build_towers
+  end
 
-def print_board (tow1, tow2, tow3)
-  puts tow1.to_s
-  puts tow2.to_s
-  puts tow3.to_s
-end
+  def get_input
+    puts "Move from which tower?"
+    from = gets.chomp.to_i
+    puts "Move to which tower?"
+    to = gets.chomp.to_i
 
-def my_transpose(matrix)
-  final = []
-  matrix.each_index do |i|
-    column = []
-    matrix.each do |row|
-      column << row[i]
+    [towers[from], towers[to]]
+  end
+
+  def move!(from, to)
+    to << from.pop
+  end
+
+  def move(from, to)
+    raise InvalidMoveError unless valid_move?(from, to)
+    move!(from, to)
+  end
+
+  def over?
+    towers.first.empty? && (towers[1].empty? || towers[2].empty?)
+  end
+
+  def play_turn
+    render
+    from, to = get_input
+    move(from, to)
+  end
+
+  def render
+    towers.each_with_index do |tower, i|
+      puts "Tower #{i}: #{tower}"
     end
-    final << column
   end
-  final   
+
+  def run
+    until over?
+      begin
+        play_turn
+      rescue InvalidMoveError
+        puts "Invalid move!"
+        retry
+      end
+    end
+    puts "You win the game!"
+  end
+
+  def valid_move?(from, to)
+    return false if from.empty?
+    to.empty? || to.last > from.last
+  end
 end
 
 def stock_picker(array)
   best_dates = []
   best_price = 0
-  
-  (0..array.length - 2).each do |i|
-    (i+1..array.length - 1).each do |j|
+
+  (0...array.length - 1).each do |i|
+    (i + 1...array.length).each do |j|
       if array[j] - array[i] > best_price
         best_dates = [i, j]
         best_price = array[j] - array[i]
       end
     end
   end
-  
+
   if best_price <= 0
-    return "Don't buy stock, you'll lose money!"
+    raise "Don't buy stock, you'll lose money!"
   end
-  
+
   best_dates
 end
 
-puts stock_picker([1, 2, 3, 4, 200])
+if __FILE__ == $PROGRAM_NAME
+  g = TowersOfHanoi.new
+  g.run
+end
